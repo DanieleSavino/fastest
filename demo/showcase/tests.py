66b7c8c6e20f356b4dd0ecfdffa556842607e31b
DanieleSavino <daniele.savino0@gmail.com>
2026-05-01 01:40:00 +0200

    shared demo run_tests.sh

--- demo/showcase/tests.py
+++ demo/showcase/tests.py
@@ -2,8 +2,8 @@
 
 import fastest
 
-pool1 = fastest.pool("pool1", *([fastest.tests.Dinline, fastest.tests.custom_test] * 10))
-pool2 = fastest.pool("pool2", *([fastest.tests.inline, fastest.tests.custom_test] * 10))
+pool1 = fastest.pool("pool1", fastest.tests.Dinline, fastest.tests.custom_test)
+pool2 = fastest.pool("pool2", fastest.tests.inline, fastest.tests.custom_test)
 
 cmp = fastest.compare(pool1, pool2, n_repeats=5)
 cmp.report()
